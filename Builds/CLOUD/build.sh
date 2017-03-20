b594b47a26af23d309d480fcead50b03a907f516
cal-id <callum.iddon@stfc.ac.uk>
2017-03-20 09:46:36 +0000

    Added shebang for build scripts

--- Builds/CLOUD/build.sh
+++ Builds/CLOUD/build.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # Install IMB PingPong
 sudo yum -y install openmpi-devel  # install OpenMPI
 wget https://software.intel.com/sites/default/files/managed/a3/53/IMB_2017.tgz
--- Builds/JASMIN_SCARF/buildIMB.sh
+++ Builds/JASMIN_SCARF/buildIMB.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # Install IMB PingPong
 wget https://software.intel.com/sites/default/files/managed/a3/53/IMB_2017.tgz
 tar -xvf IMB_2017.tgz
