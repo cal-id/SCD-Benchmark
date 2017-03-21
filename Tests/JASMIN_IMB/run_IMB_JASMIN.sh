d29b1b1210926341dc69b8b007be82d90b5497e0
cal-id <callum.iddon@stfc.ac.uk>
2017-03-21 10:59:30 +0000

    Updated IMB on JASMIN
    1) Included time limits for overrun
    2) Added comments
    3) Changed hostgroups - for some reason the previous ones were wrong

--- Tests/JASMIN_IMB/run_IMB_JASMIN.sh
+++ Tests/JASMIN_IMB/run_IMB_JASMIN.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 # westmere48G doesn't work, removed it
-hostgroups=(haswell256G ivybridge128G broadwell256G ivybridge128G ivybridge512G ivybridge128G ivybridge2000G broadwell256G)
+hostgroups=(ivybridge512G ivybridge2000G haswell256G ivybridge128G broadwell256G)
 # Make the outputs directory if it doesn't exist
 mkdir -p $PWD/outputs
-for specificHost in ${hostgroups[@]}; do
-    for i in 1 2; do
-        for repeat in 1 2 3 4 5; do
+for specificHost in ${hostgroups[@]}; do # For each hostgroup
+    for perTile in 1 2; do # Span one or two nodes
+        for repeat in 1 2 3 4 5; do # Repeat the benchmark
 
 bsub << %EndOfInput%
 #BSUB -x
 #BSUB -q par-multi
 #BSUB -n 2
-#BSUB -R "span[ptile=$i]"
+#BSUB -R "span[ptile=$perTile]"
 #BSUB -J PingPong
 #BSUB -o $PWD/outputs/%J.out
 #BSUB -e $PWD/outputs/%J.err
-#BSUB -W 0:13
+#BSUB -W 0:30
 #BSUB -m "$specificHost"
-mpirun.lotus -prot $PWD/imb/imb/src/IMB-MPI1 -iter 1000 -msglog 0:24 -iter_policy off PingPong
+mpirun.lotus -prot $PWD/imb/imb/src/IMB-MPI1 -iter 1000 -time 70 -msglog 0:24 -iter_policy off PingPong
 %EndOfInput%
 
         done
