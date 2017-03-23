79fbae945245ef69f017fcb9c9dc0a945f21b5fa
cal-id <callum.iddon@stfc.ac.uk>
2017-03-23 10:43:49 +0000

    Removed copy of make clean

--- Tests/JASMIN_IMB/makeClean.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# remove empty error files
-for errorFile in outputs/*.err; do
-    if [ ! -s $errorFile ]; then
-        echo "Removing $errorFile"
-        rm -rf $errorFile
-    fi
-done
